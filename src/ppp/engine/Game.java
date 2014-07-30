77b6b1e99a171894a2b9b8abf26fb35290802d18
Dolphins2Go <jsantill@mines.edu>
2014-07-29 20:37:04 -0600

    First commit

---
+++ src/ppp/engine/Game.java
@@ -0,0 +1,68 @@
+package ppp.engine;
+
+import org.lwjgl.input.Keyboard;
+
+public class Game {
+
+	private Mesh		mesh;
+	private Shader		shader;
+	private Transform	transform;
+
+	public Game() {
+		mesh = new Mesh();
+		shader = new Shader();
+
+		Vertex[] vertices = new Vertex[]{new Vertex(new Vector3f(-1, -1, 0)), new Vertex(new Vector3f(0, 1, 0)), new Vertex(new Vector3f(1, -1, 0)),
+				new Vertex(new Vector3f(0, -1, 1))};
+
+		int[] indices = new int[]{0, 1, 3, 3, 1, 2, 2, 1, 0, 0, 2, 3};
+
+		mesh.addVertices(vertices, indices);
+
+		transform = new Transform();
+
+		shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vs"));
+		shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.fs"));
+		shader.compileShader();
+
+		shader.addUniform("transform");
+
+	}
+
+	public void input() {
+
+		if (Input.getKeyDown(Keyboard.KEY_UP)) {
+			System.out.println("We've just pressed up!");
+		}
+		if (Input.getKeyUp(Keyboard.KEY_UP)) {
+			System.out.println("We've just released up!");
+		}
+
+		if (Input.getMouseDown(1)) {
+			System.out.println("We've just pressed RMB @" + Input.getMousePosition());
+		}
+		if (Input.getMouseUp(1)) {
+			System.out.println("We've just released RMB @" + Input.getMousePosition());
+		}
+
+	}
+
+	float	temp	= 0.0f;
+
+	public void update() {
+		temp += Time.getDelta();
+
+		float sinTemp = (float) Math.sin(temp);
+
+		transform.setTranslation(sinTemp, 0, 0);
+		transform.setRotation(0, sinTemp * 180, 0);
+		//transform.setScale(sinTemp, sinTemp, sinTemp);
+	}
+
+	public void render() {
+		shader.bind();
+		shader.setUniform("transform", transform.getTransformation());
+		mesh.draw();
+	}
+
+}
---
+++ src/ppp/engine/Vertex.java
@@ -0,0 +1,21 @@
+package ppp.engine;
+
+public class Vertex {
+
+	public static final int	SIZE	= 3;
+
+	private Vector3f		pos;
+
+	public Vertex(Vector3f pos) {
+		this.pos = pos;
+	}
+
+	public Vector3f getPos() {
+		return pos;
+	}
+
+	public void setPos(Vector3f pos) {
+		this.pos = pos;
+	}
+
+}
